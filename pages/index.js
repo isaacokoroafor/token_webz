bc3bd5b6483640832b24567e4ea62cbc50426860
Isaac Okoroafor <isaacokoroafor@Isaacs-MacBook-Pro.local>
2022-02-21 17:57:29 +0000

    This is a new commit for what I originally planned to be an amendmend

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,53 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Toolbar } from '../components/toolbar';
+import imageUrlBuilder from '@sanity/image-url';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Home({ posts }) {
-  console.log(posts);
-  return (
-    <div>
-      <Toolbar />
-      Hello World
+  const router = useRouter();
+  const [mappedPosts, setMappedPosts] = useState([]);
+  
+  useEffect(() =>  {
+    if (posts.length) {
+      const imgBuilder = imageUrlBuilder({
+        projectId: 'tq41upk7',
+        dataset: 'production',
+    });
+
+    setMappedPosts(
+      posts.map(p => {
+        return {
+          ...p,
+          mainImage: imgBuilder.image(p.mainImage).width(500).height(250),
+        }
+      })
+    );
+    } else {
+      setMappedPosts([]);
+    }
+}, [posts]);
+
+return (
+  <div>
+    <Toolbar />
+    <div className={styles.main}>
+      <h1>Welcome to NZA</h1>
+
+      <h3>Recent Posts:</h3>
+
+      <div className={styles.feed}>
+        {mappedPosts.length ? mappedPosts.map((p, index) => (
+          <div onClick={() => router.push(`/post/${p.slug.current}`)} key={index} className={styles.post}>
+            <h3>{p.title}</h3>
+            <img className={styles.mainImage} src={p.mainImage} />
+          </div>
+        )) : <>No Posts Yet</>}
+      </div>
     </div>
-  );
+  </div>
+);
 }
 
 export const getServerSideProps = async pageContext => {
