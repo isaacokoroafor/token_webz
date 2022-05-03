caebdb6b3f7f337b00546123c34bed38ba26f163
isaacokoroafor <52671538+isaacokoroafor@users.noreply.github.com>
2022-05-03 17:01:52 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,20 @@ import '../styles/globals.scss'
 import { motion, AnimatePresence } from 'framer-motion';
 import react from 'react';
 import Header from '../components/Header';
+import Layout from '../components/Layout';
+import {Switch, Route } from 'react-router-dom';
+import Home from './Home';
 
 function MyApp({ Component, pageProps, router }) {
   return ( 
     <AnimatePresence>
-      <>
-      <Header />
-      </>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </Layout>
       <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={{
         pageInitial: {
           opacity: 0
